1688fae4526379e02ecf6e81411356081e360a7b
anas40 <manas1@myamu.ac.in>
2021-05-04 05:37:20 +0530

    header footer component

--- app/src/components/footer.js
+++ app/src/components/footer.js
@@ -7,6 +7,7 @@ import '../assets/css/footer.css';
 function Footer() {
     return (
         <footer className="footersection">
+            <div><p>created by <a href="https://github.com/anas40">Anas</a></p></div>
         </footer>
     )
 }
--- app/src/components/header.js
+++ app/src/components/header.js
@@ -1,23 +1,34 @@
 //libraries
 import React, { useState, useEffect } from "react"
-import { useRecoilState, useSetRecoilState, useRecoilValue } from 'recoil'
-
+import { useRecoilState } from 'recoil'
+import { useHistory } from 'react-router-dom';
 import { login as ll } from '../store/atoms'
+import Cookies from 'universal-cookie'
 
 import '../assets/css/header.css'
+import Axios from '../store/interceptor'
+
+const cookies = new Cookies();
 
 function Header() {
-    const [isLogin] = useRecoilState(ll)
-    
+    const [isLogin,setLogin] = useRecoilState(ll)
+    const history = useHistory();
+
     function logout() {
-        
+        Axios.get('signout').then(() => {
+            cookies.remove('jwt')
+            setLogin(false)
+            history.push('/')
+        }).catch(error => {
+            console.log(error.message)
+        })
     }
     function toLogin() {
-        
+        history.push('/login')
     }
     return (
-        <header className="App-header">
-            <div>
+        <header>
+            <div className="nav">
                 {isLogin ? <div onClick={logout}>Logout</div> : <div onClick={toLogin}>Login</div>}
             </div>
         </header>
