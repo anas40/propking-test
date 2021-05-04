64355971412ee9e2c78826607ccd64c102b2bc5f
anas40 <manas1@myamu.ac.in>
2021-05-04 07:34:33 +0530

    env added to frontend

--- app/src/store/interceptor.js
+++ app/src/store/interceptor.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
-
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/",
+    baseURL: process.env.REACT_APP_BACKEND,
     withCredentials:true
 })
 
