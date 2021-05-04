398397fad97c90b54da67521498007be337d3396
anas40 <manas1@myamu.ac.in>
2021-05-04 05:37:47 +0530

    set the router

--- app/src/App.js
+++ app/src/App.js
@@ -3,20 +3,15 @@ import { RecoilRoot } from "recoil"
 
 //components and css
 import Router from './pages/index.js'
-import Header from './components/header.js'
-import Footer from './components/footer.js'
-
 import './assets/css/main.css'
 
 function App() {
 
   return (
     <RecoilRoot>
-      <Header />
-      <main className="App">
+      <div className="App">
         <Router />
-      </main>
-      <Footer />
+      </div>
     </RecoilRoot>
   );
 }
--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -2,31 +2,39 @@
 import React, { useEffect } from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Cookies from 'universal-cookie'
-import { useRecoilValue, useSetRecoilState } from 'recoil'
+import { useSetRecoilState } from 'recoil'
 
 //components and pages
 import Home from "./home.js"
 import Login from './login.js'
+import Signup from './signup.js'
 import NotFound from './notfound.js'
+import Header from '../components/header.js'
+import Footer from '../components/footer.js'
 
 import { login as ll } from '../store/atoms'
 
-//css
-
 const cookies = new Cookies();
 
-
 function Routes() {
+    const setLogin = useSetRecoilState(ll)
+    useEffect(() => {
+        let token = cookies.get('jwt')
+        if(token) setLogin(true)
+    },[])
     return (
-        <div>
-            <Router>
+        <Router>
+            <Header />
+            <main>
                 <Switch>
                     <Route path="/login" exact component={Login} />
+                    <Route path="/signup" exact component={Signup} />
                     <Route path="/" exact component={Home} />
                     <Route component={NotFound} />
                 </Switch>
-            </Router>
-        </div>
+            </main>
+            <Footer />
+        </Router>
     )
 }
 
