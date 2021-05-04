3e50d2d81a94c7eb03253140f81954ad7798dd39
anas40 <manas1@myamu.ac.in>
2021-05-04 05:38:03 +0530

    home page done

--- app/src/pages/home.js
+++ app/src/pages/home.js
@@ -1,12 +1,40 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom'
+import React, { useState,useEffect } from 'react';
+import Axios from '../store/interceptor'
+import { login as ll,data as dt} from '../store/atoms'
 
 //icons
 import '../assets/css/footer.css';
-
+import { useRecoilState } from 'recoil';
+import Land from '../components/land'
+import bg from '../assets/background/3.png'
 function Home() {
+    const [data, setData] = useState([])
+    const [isLogin, setLogin] = useRecoilState(ll)
+    const [isLoaded,setIsLoaded] = useState(false)
+    useEffect(() => {
+        Axios.get("").then(data => {
+            console.log(data);
+            setData(data)
+        }).catch(error => {
+            console.log(error);
+        }).finally(() => {
+            setIsLoaded(true)
+        })
+    },[])
+
+    if (!isLoaded) {
+        return (<div>Loadin...</div>)
+    }
+    if (data.toString() === "") {
+        return (<div>No Data</div>)
+    }
     return (
-        <div>Home
+        <div id="box" style={{backgroundImage:`url(${bg})`}}>
+            {data.data.map(land => {
+                return (
+                    <Land key={land.id} detail={land}/>
+                )
+            })}
         </div>
     )
 }
